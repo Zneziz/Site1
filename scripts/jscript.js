692225fb902db6d80aaf6b760e14cea3e310378d
Zneziz <87618287+Zneziz@users.noreply.github.com>
2025-01-19 17:57:28 -0300

    Add files via upload

---
+++ scripts/jscript.js
@@ -0,0 +1,28 @@
+function senha() {
+    const senha1 = document.getElementById('senha1').value; 
+
+    if (senha1 === "ImAlive") {
+        window.open("unknow2.html", "_blank");
+    } else {
+      alert('Senha incorreta!');
+    }
+  }
+function senhas() {
+    const senha2 = document.getElementById('senha2').value; 
+
+    if (senha2 === "MODEERF") {
+        window.open("unknow3.html", "_blank");
+    } else {
+      alert('Senha incorreta!');
+    }
+}
+
+function senhat() {
+    const senha3 = document.getElementById('senha3').value; 
+
+    if (senha3 === "EYEEYE") {
+        window.open("end.html", "_blank");
+    } else {
+      alert('Senha incorreta!');
+    }
+}
